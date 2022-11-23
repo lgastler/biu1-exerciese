9ab75bb06a27f214d15894a9d12d1cff3b0e6d22
Lennart Gastler <lennart@lgastler.de>
2022-11-23 10:32:54 +0100

    sheet(5): finalized exercise 1.1

--- tasksheet5/onedotone.m
+++ tasksheet5/onedotone.m
@@ -1,17 +1,18 @@
+% define needed variables
 a = 1:11;
 b = transpose(-50:+10:50);
 
 c = a * b;
 C = b * a;
 
-% für a wäre auf jeden fall ein unsigned integer möglich, da nicht negativ
-% also z.B. uint8
+% for a it would be possible to use a unsigned integer because it is never
+% negativ f.e. uint8
 
 D = det(C);
 
 C_inv = inv(C);
 
-% Matrix nur invertierbar, wenn die Determinante ungleich 0 ist
+% the matrix can only be inverted if det(M) is != 0
 
 C_rank = rank(C);
 
@@ -22,6 +23,12 @@ m = size(C, 2);
 % size gibt die größe der Spalten-/Zeilen an wobei length die größere der
 % beiden zurückgibt
 
+% size has the option to return the size of row and column as a two-element
+% row vector, or the needed result row or column can be defined by the
+% second optional argument
+
+% length returns the length of a vector
+
 M = magic(length(C));
 
 d = sum(M, 1);
@@ -29,5 +36,7 @@ e = transpose(sum(M, 2));
 f = trace(M);
 s = sum(M, "all");
 
+% linespace is a function which can create a set of numbers this is f.e.
+% helpful if you need to plot some function
 a_2 = linspace(1, 11, 11);
 b_2 = linspace(-50, 50 ,10);
