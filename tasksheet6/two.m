81943b4476ce90a4eacaf4e88dc43e7eac80e9a3
Lennart Gastler <lennart@lgastler.de>
2022-11-26 13:34:39 +0100

    sheet(6): added exercise 2

---
+++ tasksheet6/two.m
@@ -0,0 +1,19 @@
+% define the known variables from the exercise
+m = [5 0 0];
+n = [2 2 0];
+o = [3 3 5];
+rho = 2.71;
+
+% calculate the size and angle using m and n
+A = norm(cross(m,n));
+phi = acosd( ...
+    dot(m, n) ...
+    / ...
+    (abs(norm(m)) * abs(norm(n))) ...
+);
+
+% calculate the volume and mass using m, n and o
+V = dot(cross(m,n), o);
+m = V * rho;
+fprintf('Size A = %g m^2\nVolume V = %g m^3\nMass m = %g kg\n', A, V, m);
+fprintf('Angle: %g°\n', phi);
